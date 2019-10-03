7f984ffd211d514cd409763576c036e553cef1d8
wang0759 <54953818+wang0759@users.noreply.github.com>
2019-10-03 19:00:41 -0400

    Update library.py

--- library.py
+++ library.py
@@ -6,5 +6,21 @@ calculateAreaOfCircle(r)
 
 convertFahrenheitToCelsius(fah)
 
+def calculateDistanceBetweenPoints(x,y, x1,y1):
+
+    distance = math.sqrt((x - x1)**2 + (y - y1)**2)
+    return distance
+  
+def calculateMpg(miles,gallons):
+    mpg = miles/gallons
+    return mpg
+  
+def calculateAreaOfCircle(r):
+    areaCircle = math.pi*r**2
+    return areaCircle
+  
+def convertFahrenheitToCelsius(fah):
+    cel = (fah - 32) * 5 / 9
+    return cel
 
 
